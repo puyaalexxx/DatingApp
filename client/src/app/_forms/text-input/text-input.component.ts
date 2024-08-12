63c2bb918db6078d1708f606e83f5c3eb59875a4
alexxx <puyaalex@gmail.com>
2024-08-12 14:01:55 +0300

    changed register form to a Reactive form with more fields

---
+++ client/src/app/_forms/text-input/text-input.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-text-input',
+  standalone: true,
+  imports: [],
+  templateUrl: './text-input.component.html',
+  styleUrl: './text-input.component.css'
+})
+export class TextInputComponent {
+
+}
---
+++ client/src/app/members/photo-editor/photo-editor.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-photo-editor',
+  standalone: true,
+  imports: [],
+  templateUrl: './photo-editor.component.html',
+  styleUrl: './photo-editor.component.css'
+})
+export class PhotoEditorComponent {
+
+}
